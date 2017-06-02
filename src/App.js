c164269e46dc84e0e9a394590a861301f80079d3
prio101 <miprio101@gmail.com>
2017-06-02 21:39:11 +0600

    Added the search filter and adding movie functionality

--- src/App.js
+++ src/App.js
@@ -2,33 +2,15 @@ import React, { Component } from 'react';
 import Header from './header/Header';
 import MovieData from './movieData/MovieData';
 import Footer from './footer/Footer';
-import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      movies: []
-    };
-
-  };
-
-  componentWillMount() {
-    const url = `https://raw.githubusercontent.com/hjorturlarsen/IMDB-top-100/master/data/movies.json`
-    axios.get(url).then( res => {
-      const movies = res.data;
-      this.setState({movies: movies})
-    });
-  }
-
   render() {
-
+    console.log(this.fetchMovies)
     return (
       <div className="App">
         <Header />
-        <MovieData movies={this.state.movies} />
+        <MovieData movies={this.props.movies} />
         <Footer />
       </div>
     );
--- src/movieData/MovieData.js
+++ src/movieData/MovieData.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
 import '../movieData/movie-data.css'
 
-class MovieData extends Component {
+class MovieData extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      search: '',
+      movies: props.movies
+    }
+  }
+
+  updateSearch(event){
+    this.setState({search: event.target.value})
+  }
+
+
+  addMovie(event){
+    event.preventDefault();
+    let id = this.state.movies.length + 1;
+    let title = this.refs.title.value;
+    let rank = this.state.movies.length + 1;
+    this.setState({
+      movies: this.state.movies.concat({id, title, rank})
+    });
+    this.refs.title.value = '';
+  }
+
   render() {
+    let filteredMovies = this.state.movies.filter(
+      (movie) => {
+        return movie.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
     return(
       <div>
-        <h1>Movie Data</h1>
+        <h1>IMDB 100 Top Movies</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+        />
+
+        <form onSubmit={this.addMovie.bind(this)}>
+          <input type="text" ref="title"/>
+          <button type="submit">Add New Movie</button>
+        </form>
+
         <div className="movie-collection">
-          { this.props.movies.map(movie =>
+          {filteredMovies.map(movie =>
             <div key={ movie.id }>
                 <h4 className="movie-title">Movie Name: { movie.title }</h4>
                 <p className="movie-rank">Movie Rank:&nbsp;{ movie.rank }</p>
-                <a
-                href={ `movie/`+ movie.id+'/edit' }
-                className="button edit">
-                Edit Movie
-                </a>
-                &nbsp;
-                <a
-                href={ `movie/`+ movie.id+'/delete' }
-                className="button delete">
-                Delete
-                </a>
             </div>
           )}
         </div>
